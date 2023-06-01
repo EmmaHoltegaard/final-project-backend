0cc0476097ea286be8ff8273068ba43569e22703
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-06-02 00:16:36 +0200

    installed npm packages and added the necessary endpoints

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/final-project";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -11,10 +17,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+
+// ROUTES:
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json(listEndpoints(app))
 });
+
+//GET endpoint: Gets list of all products
+app.get("/products", (req, res) => {
+  res.send("This is a list of all products")
+})
+
+//POST endpoint: Posts product to the database
+app.post("/products", (req, res) => {
+  // create based on schema/model
+})
+
+//GET endpoint: Gets a single product
+app.get("/products/id/:id", (req, res) => {
+  res.send("This is a single product")
+})
 
 // Start the server
 app.listen(port, () => {
